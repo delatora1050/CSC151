8a7d1581bc558e0a77c0b4724ce031a578683a5c
delatora1050 <99104936+delatora1050@users.noreply.github.com>
2024-01-23 16:51:11 +0000

    Zylabs 1.15 No Parking

---
+++ Parking.java
@@ -0,0 +1,10 @@
+public class Parking {
+    public static void main(String[] args) {
+
+        System.out.println("  NO PARKING");
+        System.out.println("2:00 - 6:00 a.m");
+        System.out.print("2:00 - 6:00 a.m");
+        
+    }
+    
+}
